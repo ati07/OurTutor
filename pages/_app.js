91ed074bb55474fe6dcaac73f074bd3119a2b8b9
Atiurrahman <atiurrahman.ansari@gmail.com>
2021-06-28 15:14:58 +0530

    fourth commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import 'tailwindcss/tailwind.css'
+// import 'tailwindcss/tailwind.css'
+import '../style/global.css'
+// global.css
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
