2482c4b57a6de5d95bf2ce38a49d3bb07e330e33
unknown <atiurrahman.ansari@gmail.com>
2021-06-28 11:57:51 +0530

    third commmit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Banner from '../Components/Banner/Banner'
 import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
 import HowItWorks from '../Components/HowItsWorks/HowItWorks'
+import WhyUs from '../Components/WhyUs/WhyUs'
 
 export default function Home() {
   return (
@@ -11,10 +12,11 @@ export default function Home() {
         <title>Tutorcept</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
      <Header/>
      <Banner/>
      <HowItWorks/>
+     <WhyUs/>
      <Footer/>
      
     </div>
